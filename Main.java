243b121e6eb4172715e9fb1410d142554442401c
dahamci <wijesiriwardanadahamci@gmail.com>
2026-05-04 14:34:11 +0530

    Implemented searchStudent feature (case-insensitive)

--- Main.java
+++ Main.java
@@ -1,7 +1,64 @@
 import java.util.ArrayList;
+
 public class Main {
 static ArrayList<String> students = new ArrayList<>();
 public static void main(String[] args) {
 System.out.println("Student Record Manager");
+
+public static void main(String[] args) {
+    System.out.println("Student Record Manager");
+
+    addStudent("Alice");
+    addStudent("Bob");
 }
+deleteStudent("Alice");
+
+countStudents();
+
+searchStudent("bob");
+updateStudent("Bob", "Charlie");
+}
+
+
+}
+
+static void addStudent(String name) {
+    students.add(name);
+    System.out.println(name + " added.");
+}
+
+static void deleteStudent(String name) {
+students.remove(name);
+System.out.println(name + " removed.");
+}
+
+static void countStudents() {
+System.out.println("Total students: " + students.size());
+}
+
+static void searchStudent(String name) {
+    boolean found = false;
+
+    for (String s : students) {
+        if (s.equalsIgnoreCase(name)) {  // case-insensitive
+            System.out.println(name + " found.");
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) {
+        System.out.println(name + " not found.");
+    }
+}
+
+static void updateStudent(String oldName, String newName) {
+    int index = students.indexOf(oldName);
+
+    if (index != -1) {
+        students.set(index, newName);
+        System.out.println(oldName + " updated to " + newName);
+    } else {
+        System.out.println(oldName + " not found.");
+    }
 }
